d11bbe696bf21b18922e4b521ac59011d053542f
VitProg <vitprog@gmail.com>
2015-07-16 17:47:25 +0300

    From PhpStrom

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'FmHYNAdSPccULH_ZIYoFoJ_N17FY5NBI',
+            'cookieValidationKey' => 'fjhc387fg7g72g12g367g',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
