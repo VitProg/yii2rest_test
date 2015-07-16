c657553504a66a7c2aa76ea8617b3eeec1e46f6d
VitProg <vitprog@gmail.com>
2015-07-16 18:20:09 +0300

    From PhpStrom

--- config/db.php
+++ config/db.php
@@ -2,8 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=test',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
+    'tablePrefix' => 'rest_',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 180,
 ];
--- migrations/m150716_151335_init_db.php
+++ migrations/m150716_151335_init_db.php
@@ -7,11 +7,17 @@ class m150716_151335_init_db extends Migration
 {
     public function up()
     {
-
+        $this->createTable('news', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . ' NOT NULL',
+            'content' => Schema::TYPE_TEXT,
+        ]);
     }
 
     public function down()
     {
+        $this->dropTable('news');
+
         echo "m150716_151335_init_db cannot be reverted.\n";
 
         return false;
