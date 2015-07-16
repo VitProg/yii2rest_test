5dc2b680c807e993a98d443e3814e7e2d266721b
VitProg <vitprog@gmail.com>
2015-07-16 18:43:46 +0300

    From PhpStrom

--- migrations/m150716_151335_init_db.php
+++ migrations/m150716_151335_init_db.php
@@ -15,7 +15,7 @@ class m150716_151335_init_db extends Migration
         }
 
         $this->createTable('{{%users}}', [
-            'id' => Schema::TYPE_PK,
+            'id' => Schema::TYPE_PK . ' UNSIGNED',
             'username' => Schema::TYPE_STRING . ' NOT NULL',
             'auth_key' => Schema::TYPE_STRING . '(32) NOT NULL',
             'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
@@ -27,7 +27,7 @@ class m150716_151335_init_db extends Migration
         ], $tableOptions);
 
         $this->createTable('{{%cars}}', [
-            'id' => Schema::TYPE_PK,
+            'id' => Schema::TYPE_PK . ' UNSIGNED',
             'user_id' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
             'model_id' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
             'name' => Schema::TYPE_STRING . '(32) NOT NULL',
@@ -37,7 +37,7 @@ class m150716_151335_init_db extends Migration
         ], $tableOptions);
 
         $this->createTable('{{%cars_models}}', [
-            'id' => Schema::TYPE_PK,
+            'id' => Schema::TYPE_PK . ' UNSIGNED',
             'name' => Schema::TYPE_STRING . '(32) NOT NULL',
             'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
             'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
