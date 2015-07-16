1ff2230a989dc20d81da3d12d3388741691105f6
VitProg <vitprog@gmail.com>
2015-07-16 18:36:04 +0300

    From PhpStrom

--- migrations/m150716_151335_init_db.php
+++ migrations/m150716_151335_init_db.php
@@ -1,22 +1,57 @@
 <?php
 
-use yii\db\Schema;
+
 use yii\db\Migration;
+use yii\db\Schema;
 
 class m150716_151335_init_db extends Migration
 {
     public function up()
     {
-        $this->createTable('news', [
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%users}}', [
             'id' => Schema::TYPE_PK,
-            'title' => Schema::TYPE_STRING . ' NOT NULL',
-            'content' => Schema::TYPE_TEXT,
-        ]);
+            'username' => Schema::TYPE_STRING . ' NOT NULL',
+            'auth_key' => Schema::TYPE_STRING . '(32) NOT NULL',
+            'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
+            'password_reset_token' => Schema::TYPE_STRING,
+            'email' => Schema::TYPE_STRING . ' NOT NULL',
+            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
+            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ], $tableOptions);
+
+        $this->createTable('{{%cars}}', [
+            'id' => Schema::TYPE_PK,
+            'user_id' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'model_id' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'name' => Schema::TYPE_STRING . '(32) NOT NULL',
+            'year' => Schema::TYPE_SMALLINT . ' UNSIGNED NOT NULL DEFAULT 0',
+            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ], $tableOptions);
+        $this->createIndex('car_user_model', '{{%cars}}', ['user_id', 'model_id'], false);
+        $this->addForeignKey('car_link_model', '{{%cars}}', 'model_id', '{{%cars_model}}', 'id');
+        $this->addForeignKey('car_link_user', '{{%cars}}', 'user_id', '{{%users}}', 'id');
+
+        $this->createTable('{{%cars_models}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => Schema::TYPE_STRING . '(32) NOT NULL',
+            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ], $tableOptions);
     }
 
     public function down()
     {
-        $this->dropTable('news');
+        $this->dropTable('{{%cars_models}}');
+        $this->dropTable('{{%cars}}');
+        $this->dropTable('{{%user}}');
 
         echo "m150716_151335_init_db cannot be reverted.\n";
 
