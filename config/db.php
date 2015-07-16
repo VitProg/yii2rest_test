2690143b00c26170f03233dad063e8c03a71df9b
VitProg <vitprog@gmail.com>
2015-07-16 18:39:14 +0300

    From PhpStrom

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=test',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'test',
+    'password' => '789456123',
     'charset' => 'utf8',
     'tablePrefix' => 'rest_',
     'enableSchemaCache' => true,
