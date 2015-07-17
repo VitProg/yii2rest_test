79da811da1b575931f4e315a16fa653f200d1eef
VitProg <vitprog@gmail.com>
2015-07-17 18:05:16 +0300

    From PhpStrom

--- modules/common/models/CarModel.php
+++ modules/common/models/CarModel.php
@@ -3,6 +3,7 @@
 namespace app\modules\common\models;
 
 use Yii;
+use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "{{%cars_models}}".
@@ -35,6 +36,40 @@ class CarModel extends \yii\db\ActiveRecord
             [['name'], 'string', 'max' => 32]
         ];
     }
+
+    /**
+     * Returns a list of behaviors that this component should behave as.
+     *
+     * Child classes may override this method to specify the behaviors they want to behave as.
+     *
+     * The return value of this method should be an array of behavior objects or configurations
+     * indexed by behavior names. A behavior configuration can be either a string specifying
+     * the behavior class or an array of the following structure:
+     *
+     * ~~~
+     * 'behaviorName' => [
+     *     'class' => 'BehaviorClass',
+     *     'property1' => 'value1',
+     *     'property2' => 'value2',
+     * ]
+     * ~~~
+     *
+     * Note that a behavior class must extend from [[Behavior]]. Behavior names can be strings
+     * or integers. If the former, they uniquely identify the behaviors. If the latter, the corresponding
+     * behaviors are anonymous and their properties and methods will NOT be made available via the component
+     * (however, the behaviors can still respond to the component's events).
+     *
+     * Behaviors declared in this method will be attached to the component automatically (on demand).
+     *
+     * @return array the behavior configurations.
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class,
+        ];
+    }
+
 
     /**
      * @inheritdoc
