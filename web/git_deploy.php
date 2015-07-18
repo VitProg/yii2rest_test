d7478e932062300347d75b02a836958994d6e8ed
vitprog <vitprog@gmail.com>
2015-07-18 19:44:44 +0300

    from PhpStorm

--- web/git_deploy.php
+++ web/git_deploy.php
@@ -5,13 +5,18 @@
  * Date: 18.07.2015
  * Time: 19:31
   */
+//
+//$ua = $_SERVER['HTTP_USER_AGENT'];
+//if ($ua !== 'GitHub-Hookshot/f5d5ca1') die();
+//
+//$data = file_get_contents('php://input');
+//$data = json_decode($data, true);
+//
+//if ($data['repository']['id'] != 39199367) {
+//    die();
+//}
 
-$ua = $_SERVER['HTTP_USER_AGENT'];
-if ($ua !== 'GitHub-Hookshot/f5d5ca1') die();
-
-$data = file_get_contents('php://input');
-$data = json_decode($data);
-var_dump($data);
+exec('git pull');
 
 
 
