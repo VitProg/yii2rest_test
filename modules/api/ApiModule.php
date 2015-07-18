d4f7a543145aa783c59d23d78b45d95d8682de60
vitprog <vitprog@gmail.com>
2015-07-19 00:00:00 +0300

    from PhpStorm

--- modules/api/ApiModule.php
+++ modules/api/ApiModule.php
@@ -2,7 +2,6 @@
 
 namespace app\modules\api;
 
-use app\modules\api\components\ApiErrorHandler;
 
 class ApiModule extends \yii\base\Module
 {
@@ -11,11 +10,5 @@ class ApiModule extends \yii\base\Module
     public function init()
     {
         parent::init();
-        \Yii::$app->user->enableSession = false;
-
-//        $handler = new ApiErrorHandler();
-//        \Yii::$app->set('errorHandler', $handler);
-//        //необходимо вызывать register, это обязательный метод для регистрации обработчика
-//        $handler->register();
     }
 }
